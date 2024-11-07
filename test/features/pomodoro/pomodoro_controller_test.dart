bf524d15cd28acd693e022f517fcef6b2844ce90
dicenull <dicenull.dev@gmail.com>
2024-11-07 20:19:09 +0900

    💚 初期化時は、ポモドーロ状態が待機状態になる

---
+++ test/features/pomodoro/pomodoro_controller_test.dart
@@ -0,0 +1,17 @@
+import 'package:app/features/pomodoro/pomodoro_controller.dart';
+import 'package:app/features/pomodoro/pomodoro_state.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../create_container_test.dart';
+
+void main() {
+  test('初期化時は、ポモドーロ状態が待機状態になる', () {
+    final container = createContainer();
+    final subsc = container.listen(pomodoroControllerProvider, (_, __) {});
+
+    expect(
+      subsc.read().status,
+      PomodoroStatus.rest,
+    );
+  });
+}
