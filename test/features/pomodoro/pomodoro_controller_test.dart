786010a7a76ce393c766822ff30f212dcf95e8d4
dicenull <dicenull.dev@gmail.com>
2024-11-10 00:19:15 +0900

    💚 ポモドーロテストの準備をまとめた

--- test/features/pomodoro/pomodoro_controller_test.dart
+++ test/features/pomodoro/pomodoro_controller_test.dart
@@ -2,13 +2,21 @@ import 'package:app/features/pomodoro/pomodoro_controller.dart';
 import 'package:app/features/pomodoro/pomodoro_state.dart';
 import 'package:fake_async/fake_async.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../create_container_test.dart';
 
 void main() {
-  test('初期化時は、ポモドーロ状態が待機状態になる', () {
+  (ProviderSubscription<PomodoroState>, PomodoroController) buildSut() {
     final container = createContainer();
     final subsc = container.listen(pomodoroControllerProvider, (_, __) {});
+    final controller = container.read(pomodoroControllerProvider.notifier);
+
+    return (subsc, controller);
+  }
+
+  test('初期化時は、ポモドーロ状態が待機状態になる', () {
+    final (subsc, _) = buildSut();
 
     expect(
       subsc.read().status,
@@ -17,10 +25,9 @@ void main() {
   });
 
   test('ポモドーロを開始すると、作業中状態になる', () {
-    final container = createContainer();
-    final subsc = container.listen(pomodoroControllerProvider, (_, __) {});
+    final (subsc, controller) = buildSut();
 
-    container.read(pomodoroControllerProvider.notifier).start();
+    controller.start();
 
     expect(
       subsc.read().status,
@@ -30,13 +37,10 @@ void main() {
 
   test('作業中から25分後に、ポモドーロが完了する', () {
     fakeAsync((clock) {
-      final container = createContainer();
-      final subsc = container.listen(pomodoroControllerProvider, (_, __) {});
-
-      final controller = container.read(pomodoroControllerProvider.notifier);
+      final (subsc, controller) = buildSut();
 
       controller.start();
-      clock.elapse(const Duration(minutes: 25, seconds: 1));
+      clock.elapse(const Duration(minutes: 25));
 
       expect(subsc.read().status, PomodoroStatus.shortBreak);
     });
