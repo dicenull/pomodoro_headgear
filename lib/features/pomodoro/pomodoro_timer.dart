290eb6c984d92bf9b64be6211196ca8bc546869b
dicenull <dicenull.dev@gmail.com>
2024-11-21 18:35:42 +0900

    👍 タイマーウィジェットを作成し、ホームに配置

--- lib/features/pomodoro/pomodoro_timer.dart
+++ lib/features/pomodoro/pomodoro_timer.dart
@@ -1,4 +1,7 @@
+import 'package:app/features/pomodoro/pomodoro_controller.dart';
+import 'package:app/features/pomodoro/pomodoro_state.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class PomodoroTimer extends HookConsumerWidget {
@@ -6,6 +9,30 @@ class PomodoroTimer extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final status =
+        ref.watch(pomodoroControllerProvider.select((v) => v.status));
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(status.label),
+        const Gap(16),
+        IconButton.filled(
+          onPressed: () {
+            if (status == PomodoroStatus.rest) {
+              ref.read(pomodoroControllerProvider.notifier).start();
+              return;
+            }
+
+            ref.read(pomodoroControllerProvider.notifier).rest();
+          },
+          icon: switch (status) {
+            PomodoroStatus.rest =>
+              const Icon(Icons.play_arrow, semanticLabel: '開始'),
+            _ => const Icon(Icons.stop, semanticLabel: '停止'),
+          },
+        ),
+      ],
+    );
   }
 }
