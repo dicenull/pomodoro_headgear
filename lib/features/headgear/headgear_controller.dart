58f27c292f78ccf5d1a23f201f65bd34b3e61882
dicenull <dicenull.dev@gmail.com>
2024-12-07 23:00:32 +0900

    👍 todo進行でポモドーロも進行する

---
+++ lib/features/headgear/headgear_controller.dart
@@ -0,0 +1,26 @@
+import 'package:app/features/headgear/headgear_state.dart';
+import 'package:app/features/pomodoro/pomodoro_controller.dart';
+import 'package:app/features/todo/todo_controller.dart';
+import 'package:app/features/todo/todo_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'headgear_controller.g.dart';
+
+@riverpod
+class HeadgearController extends _$HeadgearController {
+  @override
+  HeadgearState build() {
+    ref.listen(
+        todoControllerProvider
+            .select((v) => v.any((todo) => todo.status == TodoStatus.doing)),
+        (_, isAnyDoing) {
+      if (isAnyDoing) {
+        ref.read(pomodoroControllerProvider.notifier).start();
+      }
+    });
+
+    ref.listen(pomodoroControllerProvider, (_, __) {});
+
+    return HeadgearState();
+  }
+}
---
+++ lib/features/headgear/headgear_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'headgear_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$headgearControllerHash() =>
+    r'99f8f2de84d554b080543e59902f621804f0920b';
+
+/// See also [HeadgearController].
+@ProviderFor(HeadgearController)
+final headgearControllerProvider =
+    AutoDisposeNotifierProvider<HeadgearController, HeadgearState>.internal(
+  HeadgearController.new,
+  name: r'headgearControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$headgearControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$HeadgearController = AutoDisposeNotifier<HeadgearState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
